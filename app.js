80296b11860e5fc1061aaa402a3c956a4651cb1b
Jonathan Gärtner <jonathan@twooceans.de>
2024-01-17 14:45:35 +0100

    Layout ok, favicon working

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-function drawArt(program, canvas) {
-var ctx = canvas.getContext("2d");
+function drawArt(program, speed, canvas) {
+    var ctx = canvas.getContext("2d");
+    console.log(speed);
+    console.log(program);
+    // Set the globalAlpha to control transparency (0.0 fully transparent, 1.0 fully opaque)
+    ctx.globalAlpha = 0.5;
 
-// Set the globalAlpha to control transparency (0.0 fully transparent, 1.0 fully opaque)
-ctx.globalAlpha = 0.5;
-
-// Draw a half-transparent line
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(350, 50);
-ctx.lineWidth = 5;
-ctx.strokeStyle = "#00F"; // Blue color
-ctx.stroke();
+    // Draw a half-transparent line
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(350, 50);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#00F"; // Blue color
+    ctx.stroke();
 };
