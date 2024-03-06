04637c3368b896317e22379617283dd1397f8c5c
Jonathan Gärtner <jonathan@twooceans.de>
2024-03-06 19:46:08 +0100

    transparency and image loading

--- img_to_thread.py
+++ img_to_thread.py
@@ -1,11 +1,15 @@
 from PIL import Image, ImageOps
 from xiaolinWusLineAlgorithm import draw_line
+from queue import PriorityQueue
 import numpy as np
 
 
 class StringArt:
-    def __init__(self, nails, input_image, thickness=2): #TODO invert picture
-        self.image      = self.load_image(input_image)
+    def __init__(self, nails, input_image, thickness=2):
+        if type(input_image) == str: 
+            self.image  = self.load_image(input_image)
+        else:
+            self.image  = input_image
         self.scale      = 1-(thickness-1)/self.image.width
         self.image      = self.image.resize((round(self.image.width*self.scale), round(self.image.height*self.scale)), Image.Resampling.LANCZOS)
         self.nails      = nails
@@ -17,7 +21,6 @@ class StringArt:
 
     def load_image(self, path):
         image = Image.open(path).convert("L")  # Convert to grayscale
-        # Convert the image to a NumPy array
         return image
 
     def nailToCoordinate(self, nail):
@@ -28,25 +31,25 @@ class StringArt:
         p0 = self.nailToCoordinate(start)
         p1 = self.nailToCoordinate(end)
         if p1==p0:
-            return 0
+            return 2000
         sum = [0.0, 0.1]
-        def pixel(img, p, color, transparency):
+        def pixel(img, p, color, alpha_correction, transparency):
             sum[0] += transparency*img.getpixel(p)
             sum[1] += transparency
-        draw_line(self.image, p0, p1, 0, pixel)
+        draw_line(self.image, p0, p1, 0, 1.0, pixel)
         return sum[0]/sum[1]
 
-    def drawLine(self, start, end, color=200):
+    def drawLine(self, start, end, color=200, alpha_correction=1):
         p0 = self.nailToCoordinate(start)
         p1 = self.nailToCoordinate(end)
-        draw_line(self.image, p0, p1, color)
+        draw_line(self.image, p0, p1, color, alpha_correction)
         self.operations.append((start, end))
 
-    def tryChange(self, start, end, color=200):
+    def tryChange(self, start, end, color=200, alpha_correction=1):
         self.pending_img = self.image.copy()
         p0 = self.nailToCoordinate(start)
         p1 = self.nailToCoordinate(end)
-        draw_line(self.pending_img, p0, p1, color)
+        draw_line(self.pending_img, p0, p1, color, alpha_correction)
         self.pending_operation = (start,end)
         
         return self.pending_img
@@ -56,7 +59,7 @@ class StringArt:
         self.operations.append(self.pending_operation)
     
     def printOperations(self, file=None):
-        pass # TODO
+        pass
     
     def invert(self):
         self.image = ImageOps.invert(self.image)
